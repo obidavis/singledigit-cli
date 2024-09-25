b3f3ecee88283e870ceb18fd5b49e6d9d029a768
Obi Davis <obgdavis@gmail.com>
2024-09-25 10:32:51 +0100

    more cli features

--- src/cli/generate.hpp
+++ src/cli/generate.hpp
@@ -5,4 +5,62 @@
 #ifndef GENERATE_HPP
 #define GENERATE_HPP
 
+#include "generator.hpp"
+#include "grader.hpp"
+
+#include <future>
+
+struct generation_options {
+    int min_difficulty;
+    int max_difficulty;
+    unsigned int seed;
+    int num_puzzles;
+    int threads;
+};
+
+struct generation_result {
+    board puzzle;
+    std::vector<solution_step> solve_path;
+    grade_breakdown breakdown;
+};
+
+inline auto generate(const generation_options &options) {
+    auto generate_task = [min = options.min_difficulty, max = options.max_difficulty](unsigned int seed, unsigned int count) {
+        std::vector<generation_result> results;
+        puzzle_generator generator(seed);
+        while (results.size() < count) {
+            std::string solution = generator.generate_solution();
+            std::string puzzle = generator.generate_puzzle(solution);
+            std::vector solve_path = solve(board{puzzle}, all_strategies);
+            auto breakdown = grade(solve_path);
+            float grade = breakdown.grade();
+            if (grade >= min && grade <= max) {
+                results.push_back({board{puzzle}, solve_path, breakdown});
+            }
+        }
+        return results;
+    };
+    std::vector<std::future<std::vector<generation_result>>> futures;
+    unsigned int puzzles_per_thread = options.num_puzzles / options.threads;
+    futures.reserve(options.threads);
+    for (int i = 0; i < options.threads; ++i) {
+        futures.push_back(std::async(std::launch::async, generate_task, options.seed + i, puzzles_per_thread));
+    }
+    if (options.num_puzzles % options.threads != 0) {
+        puzzles_per_thread = options.num_puzzles % options.threads;
+        futures.push_back(std::async(std::launch::async, generate_task, options.seed + options.threads, puzzles_per_thread));
+    }
+    std::vector<generation_result> results;
+    auto now = std::chrono::high_resolution_clock::now();
+    for (auto &f : futures) {
+        auto res = f.get();
+        results.insert(results.end(), res.begin(), res.end());
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    auto elapsed = duration_cast<std::chrono::milliseconds>(end - now);
+    std::println("{} ms to join", elapsed.count());
+    return results;
+}
+
+
 #endif //GENERATE_HPP
--- src/cli/main.cpp
+++ src/cli/main.cpp
@@ -3,52 +3,110 @@
 //
 
 #include "cxxopts.hpp"
-#include "generator.hpp"
-#include "grader.hpp"
+#include "generate.hpp"
+#include "solve.hpp"
 #include <print>
 #include <chrono>
+#include <iostream>
+
+std::pair<int, int> parse_difficulty(std::string_view diff) {
+    if (diff == "kids") {
+        return {0, 25};
+    } else if (diff == "easy") {
+        return {25, 75};
+    } else if (diff == "moderate") {
+        return {75, 125};
+    } else if (diff == "tough") {
+        return {125, 200};
+    } else if (diff == "hard") {
+        return {200, std::numeric_limits<int>::max()};
+    } else if (diff == "any"){
+        return {std::numeric_limits<int>::min(), std::numeric_limits<int>::max()};
+    } else {
+        throw std::invalid_argument("Invalid difficulty: " + std::string(diff));
+    }
+}
 
 int main(int argc, char **argv) {
     cxxopts::Options options("sudokugen", "Sudoku puzzle generator and solver");
     options.add_options()
-        ("c, count", "Number of puzzles to generate", cxxopts::value<int>()->default_value("1"))
-        ("s,seed", "Random seed", cxxopts::value<int>()->default_value("0"))
-        ("n,solution", "Solution to generate puzzle from", cxxopts::value<std::string>())
-        ("v,verbose", "Verbose output", cxxopts::value<bool>()->default_value("false"))
-        ("h,help", "Print usage");
-    auto result = options.parse(argc, argv);
-    if (result.count("help")) {
-        std::println("{}", options.help());
-        return 0;
-    }
-    int seed = result["seed"].as<int>();
-    int count = result["count"].as<int>();
-    bool verbose = result["verbose"].as<bool>();
-    std::string solution = generate_complete_board(seed);
-    if (result.count("solution")) {
-        solution = result["solution"].as<std::string>();
-        // TODO: validate solution
-    }
-    if (verbose) {
-        std::println("Generating {} puzzles for solution {} with seed {}",
-            count, solution, seed);
-    }
-    puzzle_generator generator(seed);
-    for (int i = 0; i < count; i++) {
-        auto start = std::chrono::high_resolution_clock::now();
-        auto puzzle = generator.generate(solution);
-        auto end = std::chrono::high_resolution_clock::now();
-        std::println("Puzzle {}: {}", i, puzzle);
-        if (verbose) {
-            std::println("Generated in {}ms",
-                duration_cast<std::chrono::milliseconds>(end - start).count());
-            auto solution_steps = solve(board{puzzle}, all_strategies);
-            auto breakdown = grade(solution_steps);
-            std::println("Elimination points: {}", breakdown.elimination_points);
-            std::println("Solution points: {}", breakdown.solution_points);
-            std::println("Average solutions per step: {}", breakdown.average_solutions_per_step);
-            std::println("Grade: {}", breakdown.grade());
+        ("h, help", "Print help")
+        ("v, verb", "CLI Action (solve, generate)", cxxopts::value<std::string>())
+        ("p, puzzle", "Puzzle to solve", cxxopts::value<std::string>())
+        ("o, output", "Output format (plain, json)", cxxopts::value<std::string>()->default_value("plain"))
+        ("f, full-solution", "Output full solution", cxxopts::value<bool>()->default_value("false"))
+        ("s, seed", "Random seed (omit for random device)", cxxopts::value<unsigned int>())
+        ("c, count", "Number of puzzles to generate_puzzle", cxxopts::value<int>()->default_value("1"))
+        ("d, difficulty", "Difficulty of generated puzzles", cxxopts::value<std::string>()->default_value("any"))
+        ("S, solution", "Solution to generate_puzzle puzzles for", cxxopts::value<std::string>())
+        ("V, verbose", "Print additional information", cxxopts::value<bool>()->default_value("false"))
+        ("j, threads", "Concurrency", cxxopts::value<int>()->default_value("1"));
+
+    try {
+        options.parse_positional({"verb", "puzzle"});
+
+        auto result = options.parse(argc, argv);
+
+        if (result.count("help")) {
+            std::println("{}", options.help());
+            return 0;
         }
+
+        if (!result.count("verb")) {
+            std::cerr << "No action specified\n";
+            return 1;
+        }
+
+        std::string verb = result["verb"].as<std::string>();
+
+        if (verb == "generate") {
+            if (result.count("puzzle")) {
+                std::cerr << "Puzzle should not be specified for generate\n";
+                return 1;
+            }
+            unsigned seed = result.count("seed")
+                ? result["seed"].as<unsigned>()
+                : std::random_device{}();
+
+            auto [min_difficulty, max_difficulty] = parse_difficulty(result["difficulty"].as<std::string>());
+            auto now = std::chrono::high_resolution_clock::now();
+            std::vector generated = generate({
+                .min_difficulty = min_difficulty,
+                .max_difficulty = max_difficulty,
+                .seed = seed,
+                .num_puzzles = result["count"].as<int>(),
+                .threads = result["threads"].as<int>()
+            });
+            auto elapsed = duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - now);
+            for (const auto &puzzle : generated) {
+                std::println("{}", puzzle.puzzle.to_short_string());
+                std::println("\t Difficulty: {}", puzzle.breakdown.grade());
+            }
+            std::println("Generated {} puzzles in {}ms", generated.size(), elapsed.count());
+        } else if (verb == "solve") {
+            if (!result.count("puzzle")) {
+                std::cerr << "Puzzle must be specified for solve\n";
+                return 1;
+            }
+            std::string puzzle = result["puzzle"].as<std::string>();
+            if (!validate_puzzle_str(puzzle)) {
+                std::cerr << "Invalid puzzle: " << puzzle << "\n";
+                return 1;
+            }
+            auto solution = solve(puzzle);
+            for (const auto &step : solution) {
+                std::println("{}", step.to_string());
+            }
+            return 0;
+        } else {
+            std::cerr << "Unknown action: " << verb << "\n";
+            return 1;
+        }
+    } catch (const cxxopts::exceptions::exception &e) {
+        std::cerr << "Error parsing options: " << e.what() << "\n";
+        return 1;
+    } catch (const std::exception &e) {
+        std::cerr << "Error: " << e.what() << "\n";
+        return 1;
     }
-    return 0;
 }
